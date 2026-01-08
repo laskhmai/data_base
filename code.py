e06baef9f890207b7eb6ac72ba957b79c7644b74
Jayanth paluri <jayanth.com>
2026-01-07 21:37:24 -0600

    Test file

--- code.py
+++ code.py
@@ -8,3 +8,7 @@ final_df["support_owner_appsvcid"] = (
     final_df["support_owner_appsvcid"]
     .combine_first(final_df["final_app_service_id"])
 )
+# ===== OPTION A: Ensure appsvcid columns always exist =====
+for col in ["billing_owner_appsvcid", "support_owner_appsvcid"]:
+    if col not in final_df.columns:
+        final_df[col] = None
