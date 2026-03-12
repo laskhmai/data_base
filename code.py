861134b7ecd0e1cc70518734699499d05481c44f
Jayanth paluri <jayanth.com>
2026-03-12 09:07:35 -0500

    Test file

--- code.py
+++ code.py
@@ -1,1 +1,5 @@
-The Terraform run failed because the module attempted to reference data.azurerm_key_vault.this[0].id, but the Key Vault data lookup returned an empty result. Since the collection had no elements, accessing index [0] caused the Invalid index error. This indicates Terraform could not find the Key Vault using the provided name and resource group values from the template.
+From the screenshots I can partially infer the issue, although I don’t have full visibility since the run is being executed locally and I don’t have access to the environment.
+
+The Key Vault TestKV-Glapi appears to be configured with the Azure RBAC permission model, which means access policies are not available. However, the AppConfig module attempts to create azurerm_key_vault_access_policy resources and references data.azurerm_key_vault.this[0].id.
+
+Since the Key Vault is RBAC-enabled, the module may not be able to apply access policies as expected, which could cause the Key Vault lookup to return an empty collection and lead to the Invalid index error.
