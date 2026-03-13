08e943f991448bf185abc3e834cee534a7ec3f25
Jayanth paluri <jayanth.com>
2026-03-13 07:38:47 -0500

    Test file

--- code.py
+++ code.py
@@ -13,3 +13,27 @@ variable "team-spn-client-secret" {
 variable "usr-dynatrace-networkzone-centralus" {
   type = string
 }
+
+variable "usr-jfrog-svc-acc-token" {
+  type = string
+}
+
+variable "usr-Humana-Enterprise-Root-CA-thumb" {
+  type = string
+}
+
+variable "usr-dynatrace-networkzone-eastus2" {
+  type = string
+}
+
+variable "usr-dynatrace-tenantid" {
+  type = string
+}
+
+variable "usr-dynatrace-server-url" {
+  type = string
+}
+
+variable "usr-dynatrace-token" {
+  type = string
+}
