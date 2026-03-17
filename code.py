681e8ef5ffde49e11c8ff2a5ff15571750013074
Jayanth paluri <jayanth.com>
2026-03-17 08:05:45 -0500

    Test file

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
-Hi Juzer, good morning. Since our Terraform structure expects both NPE and PROD components to follow the same template, we can keep the existing logic and handle this in `locals`. If the storage account is required only in NPE, we can define it under `local.npe.storage` and keep the PROD section empty, like:
+Hi Sonu, thanks for checking with **13.0.18**.
 
-prd = {
-storage = {}
-}
+Yes, the earlier issue was due to **module 13.0.11 using the old `log` block**, which is not compatible with the AzureRM **v4 provider** used in the pipeline. In **13.0.18 this was updated to `enabled_log`, so the plan runs successfully now.**
 
-This way, when the NPE workflow runs it will create the storage account, and when the PROD workflow runs no resource will be created.
+I noticed the plan is showing **2 resources to add** (`vnet-integration` and `linux-webapp-private-endpoint`). Since we didn’t add new code on our side, it’s likely Terraform is detecting them due to **module version/state differences** after switching to 13.0.18.
+
+Before applying, it would be good to verify whether those resources already exist in Azure or if they are missing from the Terraform state, just to avoid creating duplicates.
