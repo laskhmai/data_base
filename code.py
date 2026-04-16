09cbccf0d556ae53df4f1244951f084692abf8da
Jayanth paluri <jayanth.com>
2026-04-16 09:19:34 -0500

    Test file

--- code.py
+++ code.py
@@ -1,26 +1,15 @@
-SELECT 
-    Name,
-    ClusterType,
-    CASE 
-        WHEN ConnectionStrings LIKE '%azure%'    THEN 'Azure'
-        WHEN ConnectionStrings LIKE '%gcp%'      THEN 'GCP'
-        WHEN ConnectionStrings LIKE '%mongodb.net%' 
-         AND ConnectionStrings NOT LIKE '%azure%' 
-         AND ConnectionStrings NOT LIKE '%gcp%'  THEN 'AWS'
-        WHEN ConnectionStrings IS NULL           THEN 'No Connection String'
-        ELSE 'Unknown'
-    END AS Provider,
-    CASE
-        WHEN ConnectionStrings LIKE '%eastus2%'    THEN 'East US 2'
-        WHEN ConnectionStrings LIKE '%eastus%'     THEN 'East US'
-        WHEN ConnectionStrings LIKE '%centralus%'  THEN 'Central US'
-        WHEN ConnectionStrings LIKE '%westus2%'    THEN 'West US 2'
-        WHEN ConnectionStrings LIKE '%westus%'     THEN 'West US'
-        WHEN ConnectionStrings LIKE '%us-east4%'   THEN 'GCP US East 4'
-        WHEN ConnectionStrings LIKE '%us-east1%'   THEN 'GCP US East 1'
-        WHEN ConnectionStrings LIKE '%us-east%'    THEN 'AWS US East'
-        WHEN ConnectionStrings IS NULL             THEN 'No Connection String'
-        ELSE 'Unknown'
-    END AS Region
-FROM [MongoDB].[Clusters]
-ORDER BY Provider, Region
+# Upload gatewayInstall.ps1 to storage account for SHIR
+resource "null_resource" "upload_gateway_script" {
+  provisioner "local-exec" {
+    command = <<EOT
+      az storage blob upload \
+        --account-name azstoragetest11201 \
+        --container-name npeexamplecontainer1 \
+        --name gatewayInstall.ps1 \
+        --file ${path.module}/gatewayInstall.ps1 \
+        --overwrite \
+        --auth-mode login
+    EOT
+  }
+  depends_on = [module.storage]
+}
