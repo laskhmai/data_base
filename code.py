d1a8801a18338baff330a1693b0ab972d3273af0
Jayanth paluri <jayanth.com>
2026-04-15 21:59:02 -0500

    Test file

--- code.py
+++ code.py
@@ -1,21 +1,12 @@
 SELECT 
     Name,
     ClusterType,
-    CASE 
-        WHEN ConnectionStrings LIKE '%azure%' THEN 'Azure'
-        WHEN ConnectionStrings LIKE '%gcp%'   THEN 'GCP'
-        ELSE 'AWS'
-    END AS Provider,
-    CASE
-        WHEN ConnectionStrings LIKE '%eastus2%'    THEN 'East US 2'
-        WHEN ConnectionStrings LIKE '%eastus%'     THEN 'East US'
-        WHEN ConnectionStrings LIKE '%centralus%'  THEN 'Central US'
-        WHEN ConnectionStrings LIKE '%westus2%'    THEN 'West US 2'
-        WHEN ConnectionStrings LIKE '%westus%'     THEN 'West US'
-        WHEN ConnectionStrings LIKE '%us-east4%'   THEN 'GCP US East 4'
-        WHEN ConnectionStrings LIKE '%us-east1%'   THEN 'GCP US East 1'
-        WHEN ConnectionStrings LIKE '%us-east%'    THEN 'AWS US East'
-        ELSE 'Unknown'
-    END AS Region
+    ConnectionStrings
 FROM [MongoDB].[Clusters]
-ORDER BY Provider, Region
+WHERE CASE 
+    WHEN ConnectionStrings LIKE '%azure%' THEN 'Azure'
+    WHEN ConnectionStrings LIKE '%gcp%'   THEN 'GCP'
+    WHEN ConnectionStrings LIKE '%eastus%'  THEN 'Azure'
+    WHEN ConnectionStrings LIKE '%us-east%' THEN 'AWS'
+    ELSE 'Unknown'
+END = 'Unknown'
