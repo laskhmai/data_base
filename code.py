885affd1f81076f0978086417262ae046e561561
Jayanth paluri <jayanth.com>
2026-04-15 22:00:45 -0500

    Test file

--- code.py
+++ code.py
@@ -10,3 +10,14 @@ WHERE CASE
     WHEN ConnectionStrings LIKE '%us-east%' THEN 'AWS'
     ELSE 'Unknown'
 END = 'Unknown'
+
+
+CASE 
+    WHEN ConnectionStrings LIKE '%azure%'    THEN 'Azure'
+    WHEN ConnectionStrings LIKE '%gcp%'      THEN 'GCP'
+    WHEN ConnectionStrings LIKE '%mongodb.net%' 
+     AND ConnectionStrings NOT LIKE '%azure%' 
+     AND ConnectionStrings NOT LIKE '%gcp%'  THEN 'AWS'
+    WHEN ConnectionStrings IS NULL           THEN 'No Connection String'
+    ELSE 'Unknown'
+END AS Provider
