46fac8c9d8f980e6a12cdc87d14d35d8d75bc0dc
Jayanth paluri <jayanth.com>
2026-04-16 13:50:55 -0500

    Test file

--- code.py
+++ code.py
@@ -1,15 +1,7 @@
-# Upload gatewayInstall.ps1 to storage account for SHIR
-resource "null_resource" "upload_gateway_script" {
-  provisioner "local-exec" {
-    command = <<EOT
-      az storage blob upload \
-        --account-name azstoragetest11201 \
-        --container-name npeexamplecontainer1 \
-        --name gatewayInstall.ps1 \
-        --file ${path.module}/gatewayInstall.ps1 \
-        --overwrite \
-        --auth-mode login
-    EOT
-  }
-  depends_on = [module.storage]
+resource "azurerm_storage_blob" "gateway_script" {
+  name                   = "gatewayInstall.ps1"
+  storage_account_name   = "azstoragetest11201"
+  storage_container_name = "npeexamplecontainer1"
+  type                   = "Block"
+  source                 = "${path.module}/gatewayInstall.ps1"
 }
