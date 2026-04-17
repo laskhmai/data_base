221518644dcf5945e799d0e6553b0d8e5842b43c
Jayanth paluri <jayanth.com>
2026-04-17 10:10:51 -0500

    Test file

--- code.py
+++ code.py
@@ -1,9 +1,2 @@
 usr-tags = {
-  application-name  = "clinical-platform"
-  application-owner = "achander@humana.com"
-  environment       = "dev"
-  master-portfolio  = "Clinical"
-  privacy-level     = "internal"
-  support-group     = "SRE_Clinical_Modernization"
-  humana-resource-id = "APPSVC131886"
-}
+  
