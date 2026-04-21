df195fd10952aa51614291850da3a69b6252ba91
Jayanth paluri <jayanth.com>
2026-04-21 09:33:23 -0500

    Test file

--- code.py
+++ code.py
@@ -1,2 +1,10 @@
-usr-tags = {
-  
+ name: Delete Existing VM
+      run: |
+        az login --service-principal \
+          -u ${{ secrets.SPN_CLIENT_ID }} \
+          -p ${{ secrets.SPN_CLIENT_SECRET }} \
+          --tenant ${{ secrets.SPN_TENANT_ID }}
+        az vm delete \
+          --resource-group sandbox-adfpoc-eastus2-sbx-rg \
+          --name winvmasashir-vm-1 \
+          --yes || true
