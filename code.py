9437d4240e16a69e9108c88a97c602b30517d0f9
Jayanth paluri <jayanth.com>
2026-04-27 08:46:31 -0500

    Test file

--- code.py
+++ code.py
@@ -1,9 +1,2 @@
 import {
-  to = module.east-confluent-zk[1].module.zookeeper.sdm_resource.rack_server[0]
-  id = "rs-662f232f68bae786"
-}
-
-import {
-  to = module.east-confluent-zk[2].module.zookeeper.sdm_resource.rack_server[0]
-  id = "rs-526a13496b4876d"
-}
+  
