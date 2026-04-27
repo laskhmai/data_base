620351903595de9cf06f43232742ff0733c894ae
Jayanth paluri <jayanth.com>
2026-04-27 14:00:12 -0500

    Test file

--- code.py
+++ code.py
@@ -1,9 +1,2 @@
 import {
-  to = module.linuxvm["linuxvm5"].azurerm_network_interface.this[0]
-  id = "/subscriptions/5eb66160-a6d7-4d5f-ac96-c07e6b1c656c/resourceGroups/az3-plainidpdp-eastus2-npe-rg/providers/Microsoft.Network/networkInterfaces/az3-red-lvm-plainidpdp-eastus2-npe-nic-1"
-}
-
-import {
-  to = module.linuxvm["linuxvm5"].azurerm_managed_disk.datadisk[0]
-  id = "/subscriptions/5eb66160-a6d7-4d5f-ac96-c07e6b1c656c/resourceGroups/az3-plainidpdp-eastus2-npe-rg/providers/Microsoft.Compute/disks/az3-red-lvm-plainidpdp-eastus2-npe-datadisk-1"
-}
+  
