9b257ddb8e62c996e122e11e22c9f1d003e78e04
Jayanth paluri <jayanth.com>
2026-04-27 08:44:26 -0500

    Test file

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
-placeholders = ",".join(["?" for _ in batch])
-sql = f"DELETE FROM {table} WHERE {key} IN ({placeholders})"
-conn.execute(sql, batch)
-conn.commit()
-print(f"Deleted batch of {len(batch)} records")
+import {
+  to = module.east-confluent-zk[1].module.zookeeper.sdm_resource.rack_server[0]
+  id = "rs-662f232f68bae786"
+}
+
+import {
+  to = module.east-confluent-zk[2].module.zookeeper.sdm_resource.rack_server[0]
+  id = "rs-526a13496b4876d"
+}
