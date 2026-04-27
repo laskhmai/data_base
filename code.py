fa3e59ba0ef50a2c70778a47bfebccb9fd627c24
Jayanth paluri <jayanth.com>
2026-04-27 13:56:28 -0500

    Test file

--- code.py
+++ code.py
@@ -1,11 +1,9 @@
-Hi,
+import {
+  to = module.linuxvm["linuxvm5"].azurerm_network_interface.this[0]
+  id = "/subscriptions/5eb66160-a6d7-4d5f-ac96-c07e6b1c656c/resourceGroups/az3-plainidpdp-eastus2-npe-rg/providers/Microsoft.Network/networkInterfaces/az3-red-lvm-plainidpdp-eastus2-npe-nic-1"
+}
 
-The TFE_NPE_APPLY pipeline failed due to 3 main issues:
-
-1. SDM token is missing/expired in TFE NPE workspace — needs to be renewed
-2. Namespace variables (plainidpdp_base_namespace & azcli_base_namespace) are empty — need values added in TFE
-3. Several Azure resources exist but aren't in Terraform state — need terraform import run
-
-Can someone from the team please action these? Happy to jump on a call to walk through the details.
-
-Thanks!
+import {
+  to = module.linuxvm["linuxvm5"].azurerm_managed_disk.datadisk[0]
+  id = "/subscriptions/5eb66160-a6d7-4d5f-ac96-c07e6b1c656c/resourceGroups/az3-plainidpdp-eastus2-npe-rg/providers/Microsoft.Compute/disks/az3-red-lvm-plainidpdp-eastus2-npe-datadisk-1"
+}
