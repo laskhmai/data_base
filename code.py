c4cc171b135b36eb8a3f43aaefdff3862f86cf4f
Jayanth paluri <jayanth.com>
2026-04-27 21:10:06 -0500

    Test file

--- code.py
+++ code.py
@@ -1,2 +1,11 @@
-import {
-  
+-- Check if any Free or Flex clusters exist in ReplicationSpecs JSON
+SELECT 
+    clusterid,
+    ReplicationSpecs
+FROM [MongoDB].[Clusters]
+WHERE ReplicationSpecs LIKE '%"M0"%'
+   OR ReplicationSpecs LIKE '%"M2"%'
+   OR ReplicationSpecs LIKE '%"M5"%'
+   OR ReplicationSpecs LIKE '%"instanceSize": "M0"%'
+   OR ReplicationSpecs LIKE '%"instanceSize": "M2"%'
+   OR ReplicationSpecs LIKE '%"instanceSize": "M5"%';
