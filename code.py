7aa688f1242fa62f14accbd26981c06fc06bac24
Jayanth paluri <jayanth.com>
2026-05-07 15:29:55 -0500

    Test file

--- code.py
+++ code.py
@@ -1,6 +1,4 @@
-Hey Charles! The naming convention is mongo-{org-slug}-public-key 
-and mongo-{org-slug}-private-key.
-
-For cloudea: mongo-cloudea-public-key should be correct.
-If it's still invalid, you might be pointing to the wrong 
-Key Vault instance. Which Key Vault are you connecting to?
+auth = HTTPDigestAuth(
+    public_key,   ← mongo-cloudea-public-key
+    private_key   ← mongo-cloudea-private-key
+)
