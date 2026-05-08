00b4ac421427964d13bc8a7913040bde6cc05b56
Jayanth paluri <jayanth.com>
2026-05-08 14:16:38 -0500

    Test file

--- code.py
+++ code.py
@@ -1,14 +1,30 @@
-Reply: "Yes I can see dpk-test-cluster 
-in our Clusters table. ClustersKey = X, 
-StateName = IDLE. It should be getting 
-picked up by the processor."
-
+-- Check if sharedrefapp cluster exists at all
 SELECT 
-    ClustersKey,
-    Name,
-    ClusterId,
-    StateName,
-    CreateDate
+    ClustersKey, Name, ClusterId, StateName
 FROM [MongoDB].[Clusters]
-WHERE Name LIKE '%dpk%'
-   OR Name LIKE '%dpk-test%'
+WHERE Name LIKE '%sharedrefapp%'
+
+-- Also check what clusters we DO have for cloudea org
+SELECT 
+    c.ClustersKey, c.Name, c.StateName,
+    p.Name AS ProjectName,
+    o.Name AS OrgName
+FROM [MongoDB].[Clusters] c
+JOIN [MongoDB].[Projects] p ON p.ProjectKey = c.ProjectKey
+JOIN [MongoDB].[Organization] o ON o.OrgKey = p.OrgKey
+WHERE o.Name LIKE '%cloudea%'
+ORDER BY c.Name
+
+-- Check if sharedrefapp processes exist in our Process table
+SELECT
+    ProcessKey,
+    Name,
+    ProcessId,
+    ProcessType,
+    ClusterKey,
+    ReplicaSetName,
+    IsDeleted
+FROM [MongoDB].[Process]
+WHERE Name LIKE '%sharedrefapp%'
+   OR ProcessId LIKE '%sharedrefapp%'
+   OR ReplicaSetName LIKE '%sharedrefapp%'
