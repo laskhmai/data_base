07c549a9171c277b3c0705c37e17ddc810e33178
Jayanth paluri <jayanth.com>
2026-05-13 16:55:59 -0500

    Test file

--- code.py
+++ code.py
@@ -1,26 +1,18 @@
 SELECT 
-    CONVERT(DATE, s.[date]) as date,
-    SUM(s.amortized_spend) as raw_spend,
-    SUM(CAST(ISNULL(s.amortized_spend, 0.0) 
-        AS DECIMAL(18,8))) as current_proc_spend,
-    SUM(s.amortized_spend) - 
-    SUM(CAST(ISNULL(s.amortized_spend, 0.0) 
-        AS DECIMAL(18,8))) as difference
-FROM [Cloudability].[Daily_Spend] s
-WHERE s.vendor = 'Azure'
-AND s.[date] BETWEEN '2026-05-01' AND '2026-05-10'
-GROUP BY CONVERT(DATE, s.[date])
-ORDER BY date
-
-
-SELECT 
-    CONVERT(DATE, s.[date]) as date,
-    SUM(s.amortized_spend) as raw_spend,
-    SUM(ISNULL(s.amortized_spend, 0.0)) as fixed_spend,
-    SUM(s.amortized_spend) - 
-    SUM(ISNULL(s.amortized_spend, 0.0)) as difference
-FROM [Cloudability].[Daily_Spend] s
-WHERE s.vendor = 'Azure'
-AND s.[date] BETWEEN '2026-05-01' AND '2026-05-10'
-GROUP BY CONVERT(DATE, s.[date])
-ORDER BY date
+    a.date,
+    a.raw_spend,
+    b.silver_spend,
+    b.silver_spend - a.raw_spend AS difference
+FROM
+    (SELECT CONVERT(DATE, s.[date]) as date,
+            SUM(s.amortized_spend) as raw_spend
+     FROM [Cloudability].[Daily_Spend] s
+     WHERE s.vendor = 'Azure'
+     AND CONVERT(DATE, s.[date]) = '2026-05-10'
+     GROUP BY CONVERT(DATE, s.[date])) a
+JOIN
+    (SELECT billing_date,
+            SUM(overall_amortized_spend) as silver_spend
+     FROM [Silver].[Cloudability_Daily_Resource_Cost]
+     WHERE billing_date = '2026-05-10') b
+ON a.date = b.billing_date
