fdf33f08a16856c98c335a15e91c431900b2faba
Jayanth paluri <jayanth.com>
2026-05-13 15:39:37 -0500

    Test file

--- code.py
+++ code.py
@@ -1,27 +1,25 @@
--- Check ALL variations of Databricks/Synapse
--- in BOTH tables
+-- Compare Synapse data at resource level
 SELECT 
     'RAW' as source,
-    service_name,
-    SUM(amortized_spend) as spend
-FROM [Cloudability].[Daily_Spend]
-WHERE vendor = 'azure'
-AND [date] BETWEEN '2026-05-01' AND '2026-05-10'
-AND LOWER(service_name) LIKE '%databricks%' 
- OR LOWER(service_name) LIKE '%synapse%'
-GROUP BY service_name
+    CONVERT(DATE, s.[date]) as date,
+    s.resource_id,
+    SUM(s.amortized_spend) as spend
+FROM [Cloudability].[Daily_Spend] s
+WHERE s.vendor = 'azure'
+AND s.[date] BETWEEN '2026-05-01' AND '2026-05-10'
+AND s.service_name = 'Microsoft.Synapse'
+GROUP BY CONVERT(DATE, s.[date]), s.resource_id
 
-UNION ALL
+EXCEPT
 
 SELECT 
     'SILVER' as source,
-    service_name,
-    SUM(overall_amortized_spend) as spend
+    billing_date,
+    resource_id,
+    SUM(overall_amortized_spend)
 FROM [Silver].[Cloudability_Daily_Resource_Cost]
 WHERE vendor = 'Azure'
 AND CAST(billing_date AS DATE) 
     BETWEEN '2026-05-01' AND '2026-05-10'
-AND (LOWER(service_name) LIKE '%databricks%'
- OR LOWER(service_name) LIKE '%synapse%')
-GROUP BY service_name
-ORDER BY source, service_name
+AND service_name = 'Microsoft.Synapse'
+GROUP BY billing_date, resource_id
