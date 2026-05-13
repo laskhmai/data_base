0a47aa85b4b2283fa163c6726edee8e6835e61b2
Jayanth paluri <jayanth.com>
2026-05-13 15:18:06 -0500

    Test file

--- code.py
+++ code.py
@@ -1,40 +1,40 @@
-SELECT 
-    billing_date,
-    SUM(overall_amortized_spend) as silver_spend
+SELECT billing_date,
+       SUM(overall_amortized_spend) as silver_spend
 FROM [Silver].[Cloudability_Daily_Resource_Cost]
-WHERE billing_date BETWEEN '2026-05-01' AND '2026-05-10'
+WHERE vendor = 'Azure'
+AND CAST(billing_date AS DATE) BETWEEN '2026-05-01' AND '2026-05-10'
+AND service_name NOT IN ('Microsoft.Databricks','Microsoft.Synapse')
 GROUP BY billing_date
 ORDER BY billing_date
 
 
 
-SELECT 
-    CONVERT(DATE, s.[date]) as date,
-    SUM(s.amortized_spend) as raw_spend
+SELECT CONVERT(DATE, s.[date]) as date,
+       SUM(s.amortized_spend) as raw_spend
 FROM [Cloudability].[Daily_Spend] s
-WHERE s.vendor = 'Azure'
+WHERE s.vendor = 'azure'
 AND s.[date] BETWEEN '2026-05-01' AND '2026-05-10'
+AND s.service_name NOT IN ('Microsoft.Databricks','Microsoft.Synapse')
 GROUP BY CONVERT(DATE, s.[date])
 ORDER BY 1
 
 
-SELECT 
-    a.date,
-    a.raw_spend,
-    b.silver_spend,
-    b.silver_spend - a.raw_spend AS difference
-FROM
-    (SELECT CONVERT(DATE, s.[date]) as date,
-            SUM(s.amortized_spend) as raw_spend
-     FROM [Cloudability].[Daily_Spend] s
-     WHERE s.vendor = 'Azure'
-     AND s.[date] BETWEEN '2026-05-01' AND '2026-05-10'
-     GROUP BY CONVERT(DATE, s.[date])) a
-JOIN
-    (SELECT billing_date,
-            SUM(overall_amortized_spend) as silver_spend
-     FROM [Silver].[Cloudability_Daily_Resource_Cost]
-     WHERE billing_date BETWEEN '2026-05-01' AND '2026-05-10'
-     GROUP BY billing_date) b
+
+SELECT a.date, a.raw_spend, b.silver_spend,
+       b.silver_spend - a.raw_spend AS difference
+FROM (SELECT CONVERT(DATE, s.[date]) as date,
+             SUM(s.amortized_spend) as raw_spend
+      FROM [Cloudability].[Daily_Spend] s
+      WHERE s.vendor = 'azure'
+      AND s.[date] BETWEEN '2026-05-01' AND '2026-05-10'
+      AND s.service_name NOT IN ('Microsoft.Databricks','Microsoft.Synapse')
+      GROUP BY CONVERT(DATE, s.[date])) a
+JOIN (SELECT billing_date,
+             SUM(overall_amortized_spend) as silver_spend
+      FROM [Silver].[Cloudability_Daily_Resource_Cost]
+      WHERE vendor = 'Azure'
+      AND CAST(billing_date AS DATE) BETWEEN '2026-05-01' AND '2026-05-10'
+      AND service_name NOT IN ('Microsoft.Databricks','Microsoft.Synapse')
+      GROUP BY billing_date) b
 ON a.date = b.billing_date
 ORDER BY a.date
