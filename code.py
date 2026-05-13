43d8c60547cd9181acb3c3b6009666905e9beb66
Jayanth paluri <jayanth.com>
2026-05-13 16:27:46 -0500

    Test file

--- code.py
+++ code.py
@@ -1,16 +1,18 @@
--- Check ALL services for missing data
 SELECT 
     s.service_name,
     COUNT(*) as missing_records,
     SUM(s.amortized_spend) as missing_spend
 FROM [Cloudability].[Daily_Spend] s
 WHERE s.vendor = 'azure'
-AND s.[date] BETWEEN '2026-02-01' AND '2026-05-10'
+AND s.[date] BETWEEN '2026-05-01' AND '2026-05-10'
 AND NOT EXISTS (
     SELECT 1 
     FROM [Silver].[Cloudability_Daily_Resource_Cost] t
     WHERE t.billing_date = CONVERT(DATE, s.[date])
-    AND t.resource_id = s.resource_id
+    AND t.resource_id = STUFF(s.resource_id, 1,
+        CASE WHEN CHARINDEX('/', s.resource_id) > 0 
+        THEN CHARINDEX('/', s.resource_id) - 1 
+        ELSE 0 END, '')
     AND t.vendor = 'Azure'
 )
 GROUP BY s.service_name
