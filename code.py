0dd7ceb0b029299f90f621bcac177cc5264930c6
Jayanth paluri <jayanth.com>
2026-05-14 07:56:40 -0500

    Test file

--- code.py
+++ code.py
@@ -1,18 +1,13 @@
-SELECT 
-    a.date,
-    a.raw_spend,
-    b.silver_spend,
-    b.silver_spend - a.raw_spend AS difference
-FROM
-    (SELECT CONVERT(DATE, s.[date]) as date,
-            SUM(s.amortized_spend) as raw_spend
-     FROM [Cloudability].[Daily_Spend] s
-     WHERE s.vendor = 'Azure'
-     AND CONVERT(DATE, s.[date]) = '2026-05-10'
-     GROUP BY CONVERT(DATE, s.[date])) a
-JOIN
-    (SELECT billing_date,
-            SUM(overall_amortized_spend) as silver_spend
-     FROM [Silver].[Cloudability_Daily_Resource_Cost]
-     WHERE billing_date = '2026-05-10') b
-ON a.date = b.billing_date
+ALTER PROC [Silver].[usp_CloudabilityAggregate_DailySpend]
+    @UsageDate DATE = NULL  -- Add this line
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    -- Change this:
+    -- DECLARE @UsageDate DATE = DATEADD(DAY,-3,CAST(GETDATE() AS DATE))
+    
+    -- To this:
+    IF @UsageDate IS NULL
+        SET @UsageDate = 
+            DATEADD(DAY,-3,CAST(GETDATE() AS DATE))
