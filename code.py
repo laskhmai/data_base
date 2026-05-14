aadc7f0ce9464ce01ce7f3abc1277efd712cb96f
Jayanth paluri <jayanth.com>
2026-05-14 11:41:33 -0500

    Test file

--- code.py
+++ code.py
@@ -1,19 +1,15 @@
-SELECT 
-    STUFF(resource_id, 1,
-        CASE WHEN CHARINDEX('/', resource_id) > 0 
-        THEN CHARINDEX('/', resource_id) - 1 
-        ELSE 0 END, '') as resource_id,
-    COUNT(DISTINCT service_name) as service_name_count,
-    MAX(service_name) as max_service_name,
-    MIN(service_name) as min_service_name,
-    SUM(amortized_spend) as total_spend
+SELECT SUM(amortized_spend) as raw_excluded
 FROM [Cloudability].[Daily_Spend]
 WHERE vendor = 'azure'
 AND CONVERT(DATE,[date]) = '2026-05-09'
-GROUP BY 
-    STUFF(resource_id, 1,
-        CASE WHEN CHARINDEX('/', resource_id) > 0 
-        THEN CHARINDEX('/', resource_id) - 1 
-        ELSE 0 END, '')
-HAVING COUNT(DISTINCT service_name) > 1
-ORDER BY SUM(amortized_spend) DESC
+AND service_name IN 
+('Microsoft.Databricks','Microsoft.Synapse')
+
+
+
+SELECT SUM(overall_amortized_spend) as silver_excluded
+FROM [Silver].[Cloudability_Daily_Resource_Cost]
+WHERE vendor = 'Azure'
+AND billing_date = '2026-05-09'
+AND service_name IN 
+('Microsoft.Databricks','Microsoft.Synapse')
