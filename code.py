1a9c4f5829da0d276f0b385cb81c9eb8a6ad03d6
Jayanth paluri <jayanth.com>
2026-05-14 11:36:59 -0500

    Test file

--- code.py
+++ code.py
@@ -1,29 +1,19 @@
-SELECT date, SUM(amortized_spend)
-FROM cloudability.daily_spend
-WHERE vendor='azure'
-AND date BETWEEN '2026-05-01' AND '2026-05-10'
-AND service_name NOT IN 
-    ('Microsoft.Databricks','Microsoft.Synapse')
-GROUP BY date
-
-
-SELECT billing_date, SUM(overall_amortized_spend)
-FROM [Silver].[Cloudability_Daily_Resource_Cost]
-WHERE vendor='Azure'
-AND billing_date BETWEEN '2026-05-01' AND '2026-05-10'
-AND service_name NOT IN 
-    ('Microsoft.Databricks','Microsoft.Synapse')
-GROUP BY billing_date
-
-
--- Check ONE specific resource 
--- that's causing the difference
 SELECT 
-    s.resource_id,
-    SUM(s.amortized_spend) as raw_spend,
-    s.service_name
-FROM [Cloudability].[Daily_Spend] s
-WHERE s.vendor = 'azure'
-AND CONVERT(DATE,s.[date]) = '2026-05-09'
-GROUP BY s.resource_id, s.service_name
-ORDER BY SUM(s.amortized_spend) DESC
+    STUFF(resource_id, 1,
+        CASE WHEN CHARINDEX('/', resource_id) > 0 
+        THEN CHARINDEX('/', resource_id) - 1 
+        ELSE 0 END, '') as resource_id,
+    COUNT(DISTINCT service_name) as service_name_count,
+    MAX(service_name) as max_service_name,
+    MIN(service_name) as min_service_name,
+    SUM(amortized_spend) as total_spend
+FROM [Cloudability].[Daily_Spend]
+WHERE vendor = 'azure'
+AND CONVERT(DATE,[date]) = '2026-05-09'
+GROUP BY 
+    STUFF(resource_id, 1,
+        CASE WHEN CHARINDEX('/', resource_id) > 0 
+        THEN CHARINDEX('/', resource_id) - 1 
+        ELSE 0 END, '')
+HAVING COUNT(DISTINCT service_name) > 1
+ORDER BY SUM(amortized_spend) DESC
