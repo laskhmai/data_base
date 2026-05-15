1affc5d00d720657da5b5b3ca2ba8f74158b8aee
Jayanth paluri <jayanth.com>
2026-05-15 13:57:28 -0500

    Test file

--- code.py
+++ code.py
@@ -1,12 +1,6 @@
--- Test COALESCE directly on the exact ClusterKeys used
-SELECT 
-    ClustersKey,
-    Name,
-    COALESCE(
-        JSON_VALUE(ReplicationSpecs, 
-            '$[0].regionConfigs[0].effectiveElectableSpecs.instanceSize'),
-        JSON_VALUE(ReplicationSpecs, 
-            '$[0].regionConfigs[0].electableSpecs.instanceSize')
-    ) AS WhatProcGets
-FROM [MongoDB].[Clusters]
-WHERE ClustersKey IN (251, 250, 248, 252)
+COALESCE(
+    JSON_VALUE(cl.ReplicationSpecs,
+        '$[0].regionConfigs[0].effectiveElectableSpecs.instanceSize'),
+    JSON_VALUE(cl.ReplicationSpecs,
+        '$[0].regionConfigs[0].electableSpecs.instanceSize')
+) AS InstanceSize,
