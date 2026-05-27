ec7ccf86015911ee1e98d06e7326a56cf4735bb1
Jayanth paluri <jayanth.com>
2026-05-27 11:43:14 -0500

    Test file

--- code.py
+++ code.py
@@ -1,34 +1,31 @@
--- Add ConnectionLimit to MetaConfig
-ALTER TABLE [Analytics].[MongoDBMetaConfig]
-ADD ConnectionLimit     INT     NULL;
-GO
+-- CPU P95
+ALTER TABLE [Metrics].[MongoDBRightsizingAggregatedHourly]
+ADD CpuMaxP95           FLOAT   NULL;
 
--- Populate it
-UPDATE [Analytics].[MongoDBMetaConfig]
-SET ConnectionLimit = CASE SkuName
-    WHEN 'M0'   THEN 500
-    WHEN 'M2'   THEN 500
-    WHEN 'M5'   THEN 500
-    WHEN 'M10'  THEN 1500
-    WHEN 'M20'  THEN 3000
-    WHEN 'M30'  THEN 6000
-    WHEN 'M40'  THEN 16000
-    WHEN 'M50'  THEN 32000
-    WHEN 'M60'  THEN 64000
-    WHEN 'M80'  THEN 96000
-    WHEN 'M140' THEN 96000
-    WHEN 'M200' THEN 128000
-    WHEN 'M250' THEN 128000
-    WHEN 'M300' THEN 128000
-    WHEN 'M400' THEN 128000
-    WHEN 'M600' THEN 128000
-    WHEN 'M700' THEN 128000
-    ELSE 16000
-END
+ALTER TABLE [Metrics].[MongoDBRightsizingAggregatedHourly]
+ADD CpuAvgP95           FLOAT   NULL;
+
+-- Memory %
+ALTER TABLE [Metrics].[MongoDBRightsizingAggregatedHourly]
+ADD MemResidentMaxPct   FLOAT   NULL;
+
+ALTER TABLE [Metrics].[MongoDBRightsizingAggregatedHourly]
+ADD MemResidentAvgPct   FLOAT   NULL;
+
+ALTER TABLE [Metrics].[MongoDBRightsizingAggregatedHourly]
+ADD MemResidentP95Pct   FLOAT   NULL;
+
+-- Connection %
+ALTER TABLE [Metrics].[MongoDBRightsizingAggregatedHourly]
+ADD ConnUtilizationPct  FLOAT   NULL;
 GO
 
 -- Verify
-SELECT SkuName, Tier, Provider, ConnectionLimit
-FROM [Analytics].[MongoDBMetaConfig]
-ORDER BY ConnectionLimit, Tier
+SELECT
+    ORDINAL_POSITION AS [#],
+    COLUMN_NAME,
+    DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'MongoDBRightsizingAggregatedHourly'
+ORDER BY ORDINAL_POSITION
 GO
