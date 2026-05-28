61e3c41d9e86310decc7f27f32a8b8e82fa56df7
Jayanth paluri <jayanth.com>
2026-05-28 08:09:15 -0500

    Test file

--- code.py
+++ code.py
@@ -1,29 +1,113 @@
--- Check 15M table — how many readings per key per hour?
-SELECT TOP 20
-    [key],
-    CAST(DateTime AS DATE)      AS [Date],
-    DATEPART(HOUR, DateTime)    AS [Hour],
-    COUNT(*)                    AS ReadingsPerHour
-FROM [Metrics].[MongoDB_System_Normalized_Cpu_User_Max_15M]
-WHERE DateTime >= DATEADD(DAY, -1, GETDATE())
-GROUP BY
-    [key],
-    CAST(DateTime AS DATE),
-    DATEPART(HOUR, DateTime)
-ORDER BY ReadingsPerHour DESC
-GO
+# ============================================
+# Import blocks for existing KV access policies
+# ============================================
 
--- Check 5M table — should have MORE readings
-SELECT TOP 20
-    [key],
-    CAST(DateTime AS DATE)      AS [Date],
-    DATEPART(HOUR, DateTime)    AS [Hour],
-    COUNT(*)                    AS ReadingsPerHour
-FROM [Metrics].[MongoDB_System_Normalized_Cpu_User_Max_5M]
-WHERE DateTime >= DATEADD(DAY, -1, GETDATE())
-GROUP BY
-    [key],
-    CAST(DateTime AS DATE),
-    DATEPART(HOUR, DateTime)
-ORDER BY ReadingsPerHour DESC
-GO
+import {
+  to = module.functionapp-windows["remainderv3_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/46e94548-93d7-44b3-aef8-bfe7713eb44e"
+}
+
+import {
+  to = module.functionapp-windows["remainderfa_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/afaccf8b-6f4b-4406-b7dc-acb1f09f22cd"
+}
+
+import {
+  to = module.functionapp-windows["CMSNotificationfa_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/d42a956d-4fb6-4dab-985b-3e44ddb99581"
+}
+
+import {
+  to = module.functionapp-windows["communicationfa_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/183d9561-050b-46df-a55a-a75159ded24d"
+}
+
+import {
+  to = module.functionapp-windows["templatefa_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/c7a3f548-0769-497d-934c-6e9f0300cf17"
+}
+
+import {
+  to = module.functionapp-windows["businessfa_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/3b8fe1e9-87d1-44bf-93fb-7dad9e329f52"
+}
+
+import {
+  to = module.functionapp-windows["communicationfa_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/1ef1760a-716b-4e63-98a8-449f330290a9"
+}
+
+import {
+  to = module.functionapp-windows["CMSnotificationv3_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/2628c2a9-80b4-40a0-8cec-4898b093413e"
+}
+
+import {
+  to = module.functionapp-windows["templatefa_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/ac7e4f0d-f1b3-42b6-8249-1c02c6be6061"
+}
+
+import {
+  to = module.functionapp-windows["remainderfa_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/3a9336a0-2140-48aa-9344-515c3250bf7e"
+}
+
+import {
+  to = module.functionapp-windows["businessv3_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/ef790c0c-67b7-4617-b9e6-2b9cfd931fd3"
+}
+
+import {
+  to = module.functionapp-windows["CMSNotificationfa_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/09978461-c1a5-4d3e-b49e-2d0c6de43750"
+}
+
+import {
+  to = module.functionapp-windows["dispositionv3_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/44f57501-2ea0-4f05-9cbe-922ff89ec11c"
+}
+
+import {
+  to = module.functionapp-windows["businessfa_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/44ce051e-48ea-49e0-a80c-21676a69dcbe"
+}
+
+import {
+  to = module.functionapp-windows["dispositionfa_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/d62f3954-77fa-4640-aa9c-60b3dd24fda4"
+}
+
+import {
+  to = module.functionapp-windows["CMSnotificationv3_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/9b0ff7e3-e747-450d-92e6-3049b7fb9ec2"
+}
+
+import {
+  to = module.functionapp-windows["templatev3_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/74ecb012-a936-4489-afb2-55a9bf793383"
+}
+
+import {
+  to = module.functionapp-windows["businessv3_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/7479e13a-e371-41e8-aaee-51dbe494e791"
+}
+
+import {
+  to = module.functionapp-windows["remainderv3_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/c32cd786-9e18-40cc-893b-746a97952420"
+}
+
+import {
+  to = module.functionapp-windows["dispositionv3_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/a6242599-eb57-4db5-b3c4-5cf2de1afaa0"
+}
+
+import {
+  to = module.functionapp-windows["dispositionfa_central"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/449ab015-b839-44b0-ab64-5a9378d5c9fe"
+}
+
+import {
+  to = module.functionapp-windows["templatev3_east"].azurerm_key_vault_access_policy.dynatrace-token-kv-policy[0]
+  id = "/subscriptions/df5aca3c-c478-4d84-b8fc-c10132d9e7a0/resourceGroups/macs-ens-eastus2-npe-rg/providers/Microsoft.KeyVault/vaults/az3-qAcrvvVNzoiz-npe-kv/objectId/24cc39cd-06ef-4ad3-8610-926ff975a904"
+}
