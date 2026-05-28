2e80fd043e5ca277fd10acf8f168f93c25ddd0b9
Jayanth paluri <jayanth.com>
2026-05-28 13:03:39 -0500

    Test file

--- code.py
+++ code.py
@@ -1,6 +1,15 @@
-SELECT SkuName, Provider, Region, ConnectionLimit
-FROM [Analytics].[MongoDBMetaConfig]
-WHERE SkuName LIKE '%M30%'
-OR    SkuName LIKE '%M40%'
-ORDER BY SkuName
+-- What SKU/Provider/Region is cdr-uat actually using?
+SELECT
+    cl.Name         AS ClusterName,
+    JSON_VALUE(cl.ReplicationSpecs,
+        '$[0].regionConfigs[0].effectiveElectableSpecs.instanceSize')
+                    AS InstanceSize,
+    JSON_VALUE(cl.ReplicationSpecs,
+        '$[0].regionConfigs[0].providerName')
+                    AS Provider,
+    JSON_VALUE(cl.ReplicationSpecs,
+        '$[0].regionConfigs[0].regionName')
+                    AS Region
+FROM [MongoDB].[Clusters] cl
+WHERE cl.Name = 'cdr-uat'
 GO
