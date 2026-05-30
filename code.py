4747bf9eb8e1e14cd9e694f4ee814b31de591f03
Jayanth paluri <jayanth.com>
2026-05-30 10:00:54 -0500

    Test file

--- code.py
+++ code.py
@@ -1,24 +1,57 @@
--- Does cwh-cp-mgmt-prod have 2 rows in Clusters table?
 SELECT
     ClustersKey,
     Name,
-    COUNT(*) AS RowCount
+    JSON_VALUE(ReplicationSpecs,
+        '$[0].regionConfigs[0].regionName')   AS PrimaryRegion,
+    JSON_VALUE(ReplicationSpecs,
+        '$[0].regionConfigs[0].providerName')  AS Provider,
+    JSON_VALUE(ReplicationSpecs,
+        '$[0].regionConfigs[0].effectiveElectableSpecs.instanceSize') AS EffectiveSize,
+    JSON_VALUE(ReplicationSpecs,
+        '$[0].regionConfigs[0].electableSpecs.instanceSize') AS ElectableSize,
+    COUNT(*) OVER() AS RowCount
 FROM [MongoDB].[Clusters]
-WHERE Name = 'cwh-cp-mgmt-prod'
-GROUP BY ClustersKey, Name
+WHERE Name = 'consumer-interops-uat'
 GO
 
--- What region does cwh-cp-mgmt-prod actually use?
 SELECT
-    cl.Name,
-    JSON_VALUE(cl.ReplicationSpecs,
-        '$[0].regionConfigs[0].regionName')  AS PrimaryRegion,
-    JSON_VALUE(cl.ReplicationSpecs,
-        '$[0].regionConfigs[0].providerName') AS Provider,
-    JSON_VALUE(cl.ReplicationSpecs,
-        '$[0].regionConfigs[0].effectiveElectableSpecs.instanceSize') AS EffectiveSize,
-    JSON_VALUE(cl.ReplicationSpecs,
-        '$[0].regionConfigs[0].electableSpecs.instanceSize') AS ElectableSize
-FROM [MongoDB].[Clusters] cl
-WHERE cl.Name = 'cwh-cp-mgmt-prod'
+    p.ProcessType,
+    COUNT(*) AS ProcessCount
+FROM [MongoDB].[Process] p
+JOIN [MongoDB].[Clusters] cl
+    ON cl.ClustersKey = p.ClusterKey
+WHERE cl.Name     = 'consumer-interops-uat'
+AND   p.IsDeleted = 0
+GROUP BY p.ProcessType
+ORDER BY ProcessCount DESC
+GO
+
+SELECT
+    p.ProcessType,
+    MAX(cn.Measurement) AS MaxConnections,
+    AVG(cn.Measurement) AS AvgConnections,
+    COUNT(DISTINCT p.ProcessId) AS ProcessCount
+FROM [Metrics].[MongoDB_Connections_15M] cn
+JOIN [MongoDB].[Process] p
+    ON p.ProcessId = cn.[key]
+JOIN [MongoDB].[Clusters] cl
+    ON cl.ClustersKey = p.ClusterKey
+WHERE cl.Name     = 'consumer-interops-uat'
+AND   p.IsDeleted = 0
+AND   cn.DateTime >= DATEADD(DAY, -7, GETDATE())
+GROUP BY p.ProcessType
+ORDER BY MaxConnections DESC
+GO
+
+SELECT
+    ClusterName,
+    InstanceSize,
+    _date,
+    _hour,
+    ConnectionsMax,
+    ConnUtilizationPct
+FROM [Metrics].[MongoDBRightsizingAggregated5Min]
+WHERE ClusterName = 'consumer-interops-uat'
+AND   ConnUtilizationPct > 100
+ORDER BY ConnUtilizationPct DESC
 GO
