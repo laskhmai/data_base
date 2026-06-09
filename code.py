c9d70634d1c25d5b1afaaf9967663f9a63964793
Jayanth paluri <jayanth.com>
2026-06-09 08:41:37 -0500

    Test file

--- code.py
+++ code.py
@@ -1,21 +1,9 @@
-SELECT
-    COUNT(DISTINCT ClusterKey)      AS ClustersAbove100Pct,
-    COUNT(DISTINCT ClusterKey) * 100.0 /
-    (SELECT COUNT(DISTINCT ClusterKey)
-     FROM [Metrics].[MongoDBRightsizingAggregated5Min]) AS PctOfTotal
-FROM [Metrics].[MongoDBRightsizingAggregated5Min]
-WHERE MemResidentMaxPct > 100
-GO
-
--- Detail per cluster
-SELECT
+-- Check raw MemAvailableMin values
+SELECT TOP 5
     ClusterName,
-    InstanceSize,
-    ROUND(MAX(MemResidentMaxPct),     2) AS MaxMemPct,
-    ROUND(AVG(MemResidentAvgPct),     2) AS AvgMemPct,
-    ROUND(MIN(MemAvailableMin)/1024,  2) AS MinFreeMemMB
+    MIN(MemAvailableMin) AS MinAvailable,
+    MAX(MemAvailableMin) AS MaxAvailable
 FROM [Metrics].[MongoDBRightsizingAggregated5Min]
-WHERE MemResidentMaxPct > 100
-GROUP BY ClusterName, InstanceSize
-ORDER BY MaxMemPct DESC
+WHERE ClusterName = 'coreapi-shared-prod'
+GROUP BY ClusterName
 GO
