9a9e8c5dcde5c9362124acb5c8474d780ee9ead3
Jayanth paluri <jayanth.com>
2026-06-23 11:14:05 -0500

    Test file

--- code.py
+++ code.py
@@ -1,16 +1,11 @@
--- Clusters with May data but NO recommendations
-SELECT DISTINCT
-    a.ClusterKey,
-    a.ClusterName,
-    a.InstanceSize,
-    a.ProviderName,
-    a.RegionName
-FROM [Metrics].[MongoDBRightsizingAggregated5Min] a
-WHERE FORMAT(a._date,'yyyy-MM') = '2026-05'
-AND   a.ClusterKey NOT IN (
-    SELECT DISTINCT ClusterKey
-    FROM [Metrics].[MongoDBRightsizingRecommendations]
-    WHERE Month = '2026-05'
-)
-ORDER BY a.InstanceSize
+-- Check which clusters have less than 3 slices
+SELECT
+    ClusterKey,
+    ClusterName,
+    COUNT(*) AS SliceCount
+FROM [Metrics].[MongoDBRightsizingRecommendations]
+WHERE Month = '2026-05'
+GROUP BY ClusterKey, ClusterName
+HAVING COUNT(*) < 3
+ORDER BY SliceCount
 GO
