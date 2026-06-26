371df3e6a8edf5e3e519b2a034cd65400cd59010
Jayanth paluri <jayanth.com>
2026-06-26 12:45:48 -0500

    Test file

--- code.py
+++ code.py
@@ -1,8 +1,21 @@
+-- Simpler: aggregated table + recommendations
 SELECT
-    ROUND(AVG(c.Measurement), 2) AS ExactCpuAvg
-FROM [Metrics].[MongoDB_System_Normalized_Cpu_User_5M] c
-JOIN [MongoDB].[Process] p
-    ON  p.ProcessId  = c.[Key]
-    AND p.ClusterKey = 330
-WHERE CAST(c.DateTime AS DATE)   = '2026-06-16'
-AND   DATEPART(HOUR,c.DateTime)  = 0
+    a.ClusterName,
+    a.MaxCpuProcessId               AS ProcessId,
+    ROUND(a.CpuAvg,    2)          AS CpuAvg,
+    ROUND(a.CpuAvgP95, 2)          AS CpuAvgP95,
+    ROUND(a.CpuMax,    2)          AS CpuMax,
+    ROUND(a.CpuMaxP95, 2)          AS CpuMaxP95,
+    r.CurrentSku,
+    r.RecommendedSku,
+    r.Action
+FROM [Metrics].[MongoDBRightsizingAggregated5Min] a
+JOIN [Metrics].[MongoDBRightsizingRecommendations] r
+    ON  r.ClusterKey = a.ClusterKey
+    AND r.Month      = FORMAT(a._date, 'yyyy-MM')
+    AND r.DayType    = a.[type]
+    AND r.HourType   = a.businessHour
+WHERE a.ClusterName = 'cmsonc-eob-prod-cluster'
+AND   FORMAT(a._date,'yyyy-MM') = '2026-05'
+ORDER BY a.CpuMax DESC
+GO
