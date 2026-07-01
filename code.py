bb93fa0c0d64b3541d2b38f6682b01c8898a3e41
Jayanth paluri <jayanth.com>
2026-07-01 13:07:42 -0500

    Test file

--- code.py
+++ code.py
@@ -3,12 +3,14 @@ SELECT
     r.CurrentSku,
     r.RecommendedSku,
     r.Action,
-    r.MaxCpuProcessId,          ← she will check this
-    r.MaxMemProcessId,          ← she will check this
-    ROUND(r.AvgCpuMax,   2)    AS AvgCpuMax,
-    ROUND(r.PeakCpuMax,  2)    AS PeakCpuMax,
-    ROUND(MAX(a.CpuMaxP95),  2) AS CpuMaxP95,
-    ROUND(MAX(a.CpuMaxP95)*2,2) AS CpuMaxP95x2,
+    -- MaxCpuProcessId comes from Aggregated table
+    -- Pick the hour where CpuMax was highest
+    MAX(a.MaxCpuProcessId)              AS MaxCpuProcessId,
+    MAX(a.MaxMemProcessId)              AS MaxMemProcessId,
+    ROUND(r.AvgCpuMax,   2)            AS AvgCpuMax,
+    ROUND(r.PeakCpuMax,  2)            AS PeakCpuMax,
+    ROUND(MAX(a.CpuMaxP95),  2)        AS CpuMaxP95,
+    ROUND(MAX(a.CpuMaxP95)*2,2)        AS CpuMaxP95x2,
     ROUND(r.EstimatedMonthlySavings,2) AS Savings,
     r.Comment
 FROM [Metrics].[MongoDBRightsizingRecommendations] r
@@ -16,18 +18,18 @@ JOIN [Metrics].[MongoDBRightsizingAggregated5Min] a
     ON  a.ClusterKey   = r.ClusterKey
     AND a.[type]       = r.DayType
     AND a.businessHour = r.HourType
+    AND FORMAT(a._date,'yyyy-MM') = r.Month
 WHERE r.Month    = '2026-06'
 AND   r.DayType  = 'Weekday'
 AND   r.HourType = 'BusinessHours'
 AND   r.ClusterName IN (
     'cwih-cp-mgmt-prod',
     'cmsonc-eob-prod-cluster',
-    '[your Downsize cluster]'   ← replace this
+    'cdr-uat'
 )
 GROUP BY
     r.ClusterName, r.CurrentSku,
     r.RecommendedSku, r.Action,
-    r.MaxCpuProcessId, r.MaxMemProcessId,
     r.AvgCpuMax, r.PeakCpuMax,
     r.EstimatedMonthlySavings, r.Comment
 ORDER BY r.ClusterName
