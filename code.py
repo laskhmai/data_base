46735a7dbe29f2e4874c42efe339d0d35ede2c5f
Jayanth paluri <jayanth.com>
2026-07-01 16:06:50 -0500

    Test file

--- code.py
+++ code.py
@@ -1,10 +1,13 @@
 GET https://cloud.mongodb.com/api/atlas/v2/groups/
     {projectId}/processes/{hostId}:{port}/measurements?
-    m=SYSTEM_MEMORY_PERCENT_USED
-    &m=SYSTEM_MEMORY_AVAILABLE
+    m=CACHE_FILL_RATIO
+    &m=DIRTY_FILL_RATIO
+    &m=SYSTEM_MEMORY_USED
     &m=SYSTEM_MEMORY_FREE
-    &m=SYSTEM_MEMORY_TOTAL
-    &m=CACHE_FILL_RATIO
-    &m=CACHE_USED_BYTES
+    &m=SYSTEM_MEMORY_CACHED
+    &m=SYSTEM_MEMORY_AVAILABLE
+    &m=SWAP_USAGE_USED
+    &m=SWAP_IO_IN
+    &m=SWAP_IO_OUT
     &granularity=PT5M
     &period=P1D
