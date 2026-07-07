b82c3c1531e424b3259e97621cb918a21f5cab54
Jayanth paluri <jayanth.com>
2026-07-07 18:40:12 -0500

    test

--- code.py
+++ code.py
@@ -1,15 +1,31 @@
+-- Compare the two active Cluster0 records
 SELECT
-    Name,
     ClustersKey,
+    Name,
     StateName,
     Paused,
-    CreateDate
+    CreateDate,
+    JSON_VALUE(ReplicationSpecs,
+        '$[0].regionConfigs[0].electableSpecs.instanceSize')
+                                    AS InstanceSize,
+    JSON_VALUE(ReplicationSpecs,
+        '$[0].regionConfigs[0].providerName')
+                                    AS Provider,
+    JSON_VALUE(ReplicationSpecs,
+        '$[0].regionConfigs[0].regionName')
+                                    AS Region,
+    JSON_VALUE(ReplicationSpecs,
+        '$[0].regionConfigs[0].autoScaling.compute.enabled')
+                                    AS AutoScale,
+    JSON_VALUE(ReplicationSpecs,
+        '$[0].regionConfigs[0].autoScaling.compute.minInstanceSize')
+                                    AS MinSku,
+    JSON_VALUE(ReplicationSpecs,
+        '$[0].regionConfigs[0].autoScaling.compute.maxInstanceSize')
+                                    AS MaxSku
 FROM [MongoDB].[Clusters]
-WHERE Name IN (
-    'HCaaS-dev',
-    'epms-ckd3-dev',
-    'Cluster0',
-    'ecr-ckd3-dev'
-)
+WHERE Name IN ('Cluster0', 'HCaaS-Dev', 'HCaaS-dev',
+               'epms-ckd3-dev', 'ecr-ckd3-dev')
+AND   Paused = 0
 ORDER BY Name, CreateDate
 GO
