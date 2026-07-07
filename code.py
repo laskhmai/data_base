1b33432fbdcb0110bcd2ecd87255daf060682710
Jayanth paluri <jayanth.com>
2026-07-07 14:29:19 -0500

    test

--- code.py
+++ code.py
@@ -1,8 +1,16 @@
--- Check when STL table rows were inserted
+-- What does aggregated table show for cmsonc-eob-prod Weekend?
 SELECT
-    MIN(AuditUtc) AS FirstInserted,
-    MAX(AuditUtc) AS LastInserted,
-    COUNT(*)      AS TotalRows
-FROM [Metrics].[MongoDBRightsizingRecommendations_STL]
-WHERE Month = '2026-06'
+    _date,
+    _hour,
+    [type],
+    businessHour,
+    ROUND(CpuMax,    2)     AS CpuMax,
+    ROUND(CpuMaxP95, 2)     AS CpuMaxP95,
+    ROUND(CpuMaxP95*2, 2)   AS CpuMaxP95x2
+FROM [Metrics].[MongoDBRightsizingAggregated5Min]
+WHERE ClusterName = 'cmsonc-eob-prod-cluster'
+AND   FORMAT(_date,'yyyy-MM') = '2026-06'
+AND   [type]       = 'Weekend'
+AND   businessHour = 'Weekend'
+ORDER BY _date, _hour
 GO
