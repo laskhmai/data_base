036db2b76ec169c2a0ebad08197bc00ab0b05eef
Jayanth paluri <jayanth.com>
2026-07-23 13:24:07 -0500

    test

--- code.py
+++ code.py
@@ -1,9 +1,16 @@
-SELECT 
-     service_name
-    , COUNT(DISTINCT resource_id) AS resource_count
-    , SUM(amortized_spend)        AS total_spend
+-- Check what columns have data for GCP
+-- Specifically resource hierarchy columns
+SELECT TOP 5
+     resource_id
+    , vendor_account_name        -- this is like GCP Project
+    , vendor_account_identifier  
+    , service_name
+    , region
+    , [Azure_Resource_Name]      -- does this have GCP data?
+    , [Azure_Resource_Group(tag11)]  -- NULL for GCP?
+    , [Humana_Application_ID]
+    , [Humana_Resource_ID(tag23)]
 FROM [Cloudability].[Daily_Spend]
 WHERE vendor = 'GCP'
+  AND service_name = 'GCP Compute Engine'  -- check biggest service
   AND date >= DATEADD(DAY, -7, GETDATE())
-GROUP BY service_name
-ORDER BY total_spend DESC
