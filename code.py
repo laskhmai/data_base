3b7ba175b1a7ffcf6c0f76f1d2897e9b7fb7931d
Jayanth paluri <jayanth.com>
2026-07-23 13:20:21 -0500

    test

--- code.py
+++ code.py
@@ -1,15 +1,9 @@
--- How many raw rows exist for this one Silver resource_id
 SELECT 
-     date
-    , resource_id
-    , vendor
-    , [Operation]
-    , amortized_spend
-    , usage_quantity
-    , usage_family
-    , service_name
+     service_name
+    , COUNT(DISTINCT resource_id) AS resource_count
+    , SUM(amortized_spend)        AS total_spend
 FROM [Cloudability].[Daily_Spend]
-WHERE vendor = 'Azure'
-  AND date = '2026-07-08'
-  AND resource_id LIKE '%/subscriptions/185d4a23-095e-4401-8e17-8a324c77c365/resourcegroups/adb-udap-cdp-prd-eastus2-rg/providers/microsoft.compute/disks/65e4465bd8174e6198af00301bc41c6e-0-scratchvolume%'
-ORDER BY [Operation]
+WHERE vendor = 'GCP'
+  AND date >= DATEADD(DAY, -7, GETDATE())
+GROUP BY service_name
+ORDER BY total_spend DESC
