df3c5fd12d37e4ef852c37503c758db44c91e80e
Jayanth paluri <jayanth.com>
2026-08-27 21:27:44 -0500

    test

--- code.py
+++ code.py
@@ -1,8 +1,7 @@
-Completed:
+terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.0"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-3/extensions/Dynatrace
 
-Built GCP anomaly detection pipeline (adapted Azure query for GCP)
-Fixed data load, memory errors, and backfilled missing dates
-Automated daily runs for GCP aggregate spend, Property table, and MongoDB inventory
-Achieved 93% accuracy on seasonality model; validated anomaly detection end-to-end
-Fixed email reporting bugs (Resource Group → Project label, text spacing)
-Built recurring cost-spike validation script (runs every 2 weeks)
+terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.1"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-4/extensions/Dynatrace
+
+terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.2"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-5/extensions/Dynatrace
+
+terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.3"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-6/extensions/Dynatrace
