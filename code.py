2398586013c752b3bde7099d3a978bbe2ad6d884
Jayanth paluri <jayanth.com>
2026-08-27 21:33:07 -0500

    test

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
-terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.0"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-3/extensions/Dynatrace
-
-terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.1"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-4/extensions/Dynatrace
-
-terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.2"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-5/extensions/Dynatrace
-
-terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.3"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-6/extensions/Dynatrace
+- name: Import orphaned Dynatrace extensions
+  working-directory: ./
+  run: |
+    terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.0"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-3/extensions/Dynatrace
+    terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.1"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-4/extensions/Dynatrace
+    terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.2"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-5/extensions/Dynatrace
+    terraform import 'module.linuxvm-dynatrace["qa-rhapsody-eus2.3"].azurerm_virtual_machine_extension.ext' /subscriptions/3a9a8766-02cf-4c9f-a4a3-8c0cd6bc3ed2/resourceGroups/hcs-clinical-platform-eastus2-qa-rg/providers/Microsoft.Compute/virtualMachines/azeus2cp-rhapsody-qa-vm-6/extensions/Dynatrace
