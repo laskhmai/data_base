47e4b8daf773e799522d24ac78c4d93ca599bf7b
Jayanth paluri <jayanth.com>
2026-09-15 14:57:02 -0500

    test

--- code.py
+++ code.py
@@ -1,8 +1,1 @@
-<<<<<<< HEAD
-cd /path/to/your/repo
-terraform init
-terraform state rm 'module.db-server["azuresql-sf-prov-stage"].azurerm_resource_group_template_deployment.automatic-tuning-arm-template'
-terraform state rm 'module.db-server["azuresql-sf-prov-stage"].azurerm_resource_group_template_deployment.security-alerts-arm-template[0]'
-=======
-Are you asking to reference our module directly from your infra repo instead of using our standard sample-template/TFC workspace setup?
->>>>>>> e5f54cf (test)
+Are you asking to reference our module directly from your infra repo instead of using our standard sample-template/TFC workspace setup?”
